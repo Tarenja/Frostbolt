cd9bc9c1b75556737e9cbf5440ea295c93fdcff3
Jolissa Latour <johanne.e.latour@gmail.com>
2017-06-23 21:40:30 +0200

    new file:   images/GoogleMaps.png 	deleted:    images/frontpagepics/Dishonored.png 	deleted:    images/frontpagepics/DragonAge.png 	deleted:    images/frontpagepics/Fallout.png 	new file:   images/frontpagepics/gentlemanbastard.png 	new file:   images/frontpagepics/lyriummadness.png 	new file:   images/frontpagepics/lyriummadnessgirledition.png 	renamed:    images/frontpagepics/Skyrim.png -> images/frontpagepics/magicvikings.png 	renamed:    images/frontpagepics/MassEffect.png -> images/frontpagepics/manvsuniverse.png 	new file:   images/frontpagepics/wildwastelandwanderer.png 	modified:   index.html 	modified:   main.css 	modified:   main.js

---
+++ main.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+
+	// everything below here is for the homepage
+
+	$('#gamesSection span').hide();
+
+
+ 	$(function () {
+ 		$('#img1').hover(function() {
+ 			$('#Fallout').fadeIn(500);
+ 		});
+ 	});
+
+ 	 	$(function () {
+  		$('#img1').mouseleave(function () {
+  			$('#Fallout').fadeOut(500);
+  		});
+ 	});
+
+ 	  	$(function () {
+ 		$('#img2').hover(function() {
+ 			$('#Dishonored').fadeIn(500);
+ 		});
+ 	});
+
+ 	 	$(function () {
+  		$('#img2').mouseleave(function () {
+  			$('#Dishonored').fadeOut(500);
+  		});
+ 	});
+
+ 	 	$(function () {
+ 		$('#img3').hover(function() {
+ 			$('#DragonAge2').fadeIn(500);
+ 		});
+ 	});
+
+ 	 	$(function () {
+  		$('#img3').mouseleave(function () {
+  			$('#DragonAge2').fadeOut(500);
+  		});
+ 	});
+
+ 	  	$(function () {
+ 		$('#img4').hover(function() {
+ 			$('#MassEffect').fadeIn(500);
+ 		});
+ 	});
+
+ 	 	$(function () {
+  		$('#img4').mouseleave(function () {
+  			$('#MassEffect').fadeOut(500);
+  		});
+ 	});
+
+ 	  	$(function () {
+ 		$('#img5').hover(function() {
+ 			$('#Skyrim').fadeIn(500);
+ 		});
+ 	});
+
+ 	 	$(function () {
+  		$('#img5').mouseleave(function () {
+  			$('#Skyrim').fadeOut(500);
+  		});
+ 	});
+
+ 	  	$(function () {
+ 		$('#img6').hover(function() {
+ 			$('#DragonAge').fadeIn(500);
+ 		});
+ 	});
+
+ 	 	$(function () {
+  		$('#img6').mouseleave(function () {
+  			$('#DragonAge').fadeOut(500);
+  		});
+ 	});
+
+
+
+});
