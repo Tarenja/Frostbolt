0d98cc6e04b17c6783da53e21ebe123d45201b62
uxster <hello@uxster.nl>
2017-06-27 14:49:55 +0200

    made some last tweaks to make the pages look good at 80% zoom on macbooks and added some jQuery interaction on the homepage

--- main.js
+++ main.js
@@ -51,6 +51,65 @@ $(document).ready(function() {
   		$('#img6').mouseleave(function () {
   			$('#DragonAge').stop().animate({opacity: '0.2'});
   		});
- 	
 
+      //gamesSectiona animations
+
+      $('#gamesSectiona span').css('opacity', '0.2');
+
+    $('#img1a').hover(function() {
+      $('#Fallouta').animate({opacity: '1'});  
+    });
+
+      $('#img1a').mouseleave(function () {
+        $('#Fallouta').stop().animate({opacity: '0.2'});
+      });
+      
+    $('#img2a').hover(function() {
+      $('#Dishonoreda').animate({opacity: '1'});
+    });
+    
+      $('#img2a').mouseleave(function () {
+        $('#Dishonoreda').stop().animate({opacity: '0.2'});
+      });
+    
+    $('#img3a').hover(function() {
+      $('#DragonAge2a').animate({opacity: '1'});
+    });
+    
+      $('#img3a').mouseleave(function () {
+        $('#DragonAge2a').stop().animate({opacity: '0.2'});
+      });
+        
+    $('#img4a').hover(function() {
+      $('#MassEffecta').animate({opacity: '1'});
+    });
+  
+      $('#img4a').mouseleave(function () {
+        $('#MassEffecta').stop().animate({opacity: '0.2'});
+      }); 
+      
+    $('#img5a').hover(function() {
+      $('#Skyrima').animate({opacity: '1'});
+    });
+    
+      $('#img5a').mouseleave(function () {
+        $('#Skyrima').stop().animate({opacity: '0.2'});
+      });
+        
+    $('#img6a').hover(function() {
+      $('#DragonAgea').animate({opacity: '1'});
+    });
+
+      $('#img6a').mouseleave(function () {
+        $('#DragonAgea').stop().animate({opacity: '0.2'});
+      });
+
+      //See-all button animations
+
+      $('#gamesSectiona').hide();
+
+      $('#see').click(function() {
+        $('#gamesSectiona').slideToggle("slow");
+      });
+ 	
 });
